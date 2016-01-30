37351e695c55e3c69e7110191615b7ef81986a49
jiefeng <jiefeng@email.arizona.edu>
2016-01-30 16:29:37 -0700

    set the user email and username

---
+++ Conditional.py
@@ -0,0 +1,12 @@
+
+x=100
+day ="Friday"
+
+
+
+if x<10:
+    print("that makes sense")
+elif day == "Friday":
+    print ("I dont like friday")
+else:
+    print("i dont think this will happen")
---
+++ Interation.py
@@ -0,0 +1,5 @@
+books = ["Tale of two cities", "Harry Potter", "Lord of the Rings"]
+nums= range(100)
+
+for a in nums:
+    print (a)
